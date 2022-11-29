43bc9a944d3aaa2d90238fe66f96156941e821f9
julianomachadoo <julmachadoramos@gmail.com>
2022-11-29 14:44:13 -0300

    Teste de repositório em banco separado, em outro profile

---
+++ src/test/java/com/github/julianomachadoo/forumapi/repository/CursoRepositoryTest.java
@@ -0,0 +1,42 @@
+package com.github.julianomachadoo.forumapi.repository;
+
+import com.github.julianomachadoo.forumapi.modelo.Curso;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
+class CursoRepositoryTest {
+
+    @Autowired
+    private CursoRepository repository;
+
+    @Autowired
+    private TestEntityManager em;
+
+    @Test
+    public void deveriaCarregarUmCursoAoBuscarPeloSeuNome() {
+        String nomeCurso = "HTML 5";
+
+        Curso html5 = new Curso(nomeCurso, "Programacao");
+        em.persist(html5);
+
+        Curso curso = repository.findByNome(nomeCurso);
+        assertNotNull(curso);
+        assertEquals(nomeCurso, curso.getNome());
+    }
+
+    @Test
+    public void naoDeveriaCarregarUmCursoCujoNomeNaoEstejaCadastrado() {
+        String nomeCurso = "JPA";
+        Curso curso = repository.findByNome(nomeCurso);
+        assertNull(curso);
+    }
+}
