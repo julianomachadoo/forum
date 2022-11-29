965a6790b6458d6226fdc3ec6a13159be206fda5
julianomachadoo <julmachadoramos@gmail.com>
2022-11-29 10:54:55 -0300

    Adicionando header Authorization no swagger

--- src/main/java/com/github/julianomachadoo/forumapi/config/swagger/SwaggerConfigurations.java
+++ src/main/java/com/github/julianomachadoo/forumapi/config/swagger/SwaggerConfigurations.java
@@ -3,10 +3,14 @@ package com.github.julianomachadoo.forumapi.config.swagger;
 import com.github.julianomachadoo.forumapi.modelo.Usuario;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Collections;
 
 @Configuration
 public class SwaggerConfigurations {
@@ -19,6 +23,13 @@ public class SwaggerConfigurations {
                 .paths(PathSelectors.ant("/**"))
                 .build()
                 .ignoredParameterTypes(Usuario.class)
-                ;
+                .globalOperationParameters(Collections.singletonList(
+                        new ParameterBuilder()
+                                .name("Authorization")
+                                .description("Header para token JWT")
+                                .modelRef(new ModelRef("string"))
+                                .parameterType("header")
+                                .required(false)
+                                .build()));
     }
 }
